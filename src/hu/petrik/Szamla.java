86d4e5c214d79d1f947d83f0bf93e254160d850e
Kristof <takats.kristof.laszlo@gmail.com>
2021-11-01 21:01:57 +0100

    Szamla osztály

---
+++ src/hu/petrik/Szamla.java
@@ -0,0 +1,18 @@
+package hu.petrik;
+
+public abstract class Szamla extends BankiSzolgaltatas{
+    private double egyenleg;
+
+    public Szamla(Tulajdonos tulajdonos){
+        super(tulajdonos);
+    }
+    public double getEgyenleg(){
+        return egyenleg;
+    }
+    public void Befizet(double osszeg){
+        if(osszeg>0){
+            this.egyenleg+=osszeg;
+        }
+    }
+    public abstract boolean kivesz(double osszeg);
+}
