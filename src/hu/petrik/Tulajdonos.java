db17f293c1678679cd28d9f60cf65e27b7906add
Kristof <takats.kristof.laszlo@gmail.com>
2021-11-01 20:44:39 +0100

    Tulajdonos osztály

---
+++ src/hu/petrik/Tulajdonos.java
@@ -0,0 +1,15 @@
+package hu.petrik;
+
+public final class Tulajdonos {
+    private String nev;
+
+    public Tulajdonos(String nev) {
+        this.nev=nev;
+    }
+    public String getNev() {
+        return nev;
+    }
+    public void setNev(String nev){
+        this.nev=nev;
+    }
+}
